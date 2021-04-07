979ab19d82de20ef562e9a551c04e16d0da58150
Daniele Alessi <daniele.alessi@hotmail.it>
2021-04-07 16:11:23 +0200

    modificato setTimeout

--- js/script.js
+++ js/script.js
@@ -20,25 +20,23 @@ console.log(numeri);
 alert(numeri);
 
 // 3 - faccio partire il timer di 30 secondi dopo l'alert.
-// salvo in una variabile i secondi di partenza
-setTimeout(function(){
-    var seconds = 5;
-    var secondsElement = document.getElementById('seconds');
+var seconds = 5;
+var secondsElement = document.getElementById('seconds');
+
+// salvo in una variabile il countdown
+var countdown = setInterval(function(){
+    // devo popolare l'elemento html ogni secondo 
+    secondsElement.innerHTML = seconds;
+    seconds--;
+
+    // quando arriva a 0 si deve fermare 
+    if (seconds === 0) {
+        clearInterval(countdown);
+        secondsElement.innerHTML = "tempo scaduto!";
+    }
     
-    // salvo in una variabile il countdown
-    var countdown = setInterval(function(){
-        // devo popolare l'elemento html ogni secondo 
-        secondsElement.innerHTML = seconds;
-        seconds--;
-    
-        // quando arriva a 0 si deve fermare 
-        if (seconds === 0) {
-            clearInterval(countdown);
-            secondsElement.innerHTML = "tempo scaduto!";
-        }
-        
-    }, 1000);
 }, 1000);
+
 
 
 
